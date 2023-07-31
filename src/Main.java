00066a28a4aed510da153401a36fba48caa162db
Abdulaziz Aljabri <abdulaziz.aljabri18@gmail.com>
2023-07-31 14:39:21 +0300

    Exercise7

--- src/Main.java
+++ src/Main.java
@@ -2,47 +2,48 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         // First Example form Class Account ...
-//        try {
-//            Account accountOne = new Account();
-//            Account accountTwo = new Account("12", "ail", 200);
-        Scanner input = new Scanner(System.in);
-//            System.out.println("Enter Your ID :");
-//            String id = input.nextLine();
-//            accountOne.setId(id);
-//            System.out.println("Enter Your Name :");
-//            String name = input.nextLine();
-//            accountOne.setName(name);
-//            System.out.println("Enter Your Balance :");
-//            int balance = input.nextInt();
-//            accountOne.setBalance(balance);
-//            System.out.println("-------------------");
-//            System.out.println("Enter the money to withdraw :");
-//            int amount = input.nextInt();
-//            accountOne.credit(amount);
-//            System.out.println("The balance after withdraw is : " + accountOne.getBalance());
-//            System.out.println("Enter the amount to debit");
-//            amount = input.nextInt();
-//            accountOne.debit(amount);
-//            System.out.println("The balance after debit is : " + accountOne.getBalance());
-//            System.out.println("-------------------");
-//            System.out.println("Enter the money to transfer :");
-//            amount = input.nextInt();
-//            accountOne.transferTo(amount, accountTwo);
-//            System.out.println("The balance your account is : " + accountOne.getBalance());
-//            System.out.println("-------------------");
-//            System.out.println("The balance of accountOne after transfer is : " + accountOne.getBalance());
-//            System.out.println("The balance of accountTwo after transfer is : " + accountTwo.getBalance());
-//            System.out.println("-------------------");
-//            System.out.println(accountOne.toString());
-//            System.out.println(accountTwo.toString());
-//        } catch (InputMismatchException e1) {
-//            System.out.println(" Invalid Input ");
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//        }
+        try {
+            Account accountOne = new Account();
+            Account accountTwo = new Account("12", "ail", 200);
+            System.out.println("Enter Your ID :");
+            String id = input.nextLine();
+            accountOne.setId(id);
+            System.out.println("Enter Your Name :");
+            String name = input.nextLine();
+            accountOne.setName(name);
+            System.out.println("Enter Your Balance :");
+            int balance = input.nextInt();
+            accountOne.setBalance(balance);
+            System.out.println("-------------------");
+            System.out.println("Enter the money to withdraw :");
+            int amount = input.nextInt();
+            accountOne.credit(amount);
+            System.out.println("The balance after withdraw is : " + accountOne.getBalance());
+            System.out.println("Enter the amount to debit");
+            amount = input.nextInt();
+            accountOne.debit(amount);
+            System.out.println("The balance after debit is : " + accountOne.getBalance());
+            System.out.println("-------------------");
+            System.out.println("Enter the money to transfer :");
+            amount = input.nextInt();
+            accountOne.transferTo(amount, accountTwo);
+            System.out.println("The balance your account is : " + accountOne.getBalance());
+            System.out.println("-------------------");
+            System.out.println("The balance of accountOne after transfer is : " + accountOne.getBalance());
+            System.out.println("The balance of accountTwo after transfer is : " + accountTwo.getBalance());
+            System.out.println("-------------------");
+            System.out.println(accountOne.toString());
+            System.out.println(accountTwo.toString());
+        } catch (InputMismatchException e1) {
+            System.out.println(" Invalid Input ");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
         // Second Example form Class Employee...
 
